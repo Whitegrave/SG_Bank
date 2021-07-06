b021111e79511abcdaad457a3c32b050b73955ec
Whitegrave <mrcraig0907@gmail.com>
2021-07-06 12:38:18 -0400

    Implemented video 4

---
+++ SG_Bank.Data/FreeAccountTestRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SG_Bank.Models;
+using SG_Bank.Models.Interfaces;
+
+namespace SG_Bank.Data
+{
+    public class FreeAccountTestRepository : IAccountRepository
+    {
+        private static Account _account = new Account
+        {
+            Name = "Free Account",
+            Balance = 100.00M,
+            AccountNumber = "12345",
+            Type = AccountType.FREE
+        };
+        public Account LoadAccount(string AccountNumber)
+        {
+            return _account;
+        }
+
+        public void SaveAccount(Account account)
+        {
+            _account = account;
+        }
+    }
+}
---
+++ SG_Bank.Models/AccountType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG_Bank.Models
+{
+    public enum AccountType
+    {
+        FREE=1,
+        BASIC,
+        PREMIUM
+    }
+}
